49c9007f95888baf4df449aa87617a97efcba439
jp2113 <jp2113@york.ac.uk>
2024-05-06 10:00:50 +0100

    Completed 2nd function, and formatting of comments

--- tidal_analysis.py
+++ tidal_analysis.py
@@ -7,68 +7,100 @@ import pandas as pd
 import glob
 
 
+filename = "data/1947ABE.txt"
+
+
 # Opens a specified file, formats columns as needed, and removes uneeded data
 def read_tidal_data(filename):
+# Creates a gloobal variable 
     global data
 # Removes unneeded data
     data = pd.read_csv(filename , sep=r"\s+", skiprows=[0,1,2,3,4,5,6,7,8,10])
-# Renames column to 'Sea Level'
-    data = data.rename(columns={data.columns[3] : 'Sea Level'})
 # Amalgamating the Date and Time column into one
     data["Datetime"] = pd.to_datetime(data['Date'] + ' ' + data['Time'])
     data = data.set_index("Datetime")
+# Renames column to 'Sea Level'
+    data = data.rename(columns={data.columns[3] : 'Sea Level'})
+# Removes uneeded columns
+    data = data.drop(columns = ['Date','Time'])
 # Replaces any value containing M,N,T with NaN, in the 'Sea Level' column
     data.replace(to_replace=".*M$",value={"Sea Level": np.nan}, regex=True, inplace=True)
     data.replace(to_replace=".*N$",value={"Sea Level": np.nan}, regex=True, inplace=True)
     data.replace(to_replace=".*T$",value={"Sea Level": np.nan}, regex=True, inplace=True)
+    print(data)
     
-    return data
+    return(data)
+
+read_tidal_data(filename)
 
 
+
+# Creates new df with specified year, and subtracts the mean from each data point
 def extract_single_year_remove_mean(year, data):
-    data['Date'] = pd.DatetimeIndex(data['Date']).year
-    year_data = data['Date']
-    global year_data_start
-    global year_data_end
-    year_data_start = str(year) + '0101'
-    year_data_end = str(year) + '1231'
-    # remove mean to oscillate around zero
-    mmm = np.mean(year_data['Sea Level'])
-    year_data['Sea Level'] -= mmm
+# Creates a global variable for viewing purposes
+    global year_data
+# Sets start and endpoints of the year
+    year_start = str(year) + "0101"
+    year_end = str(year) + "1231"
+# Siphones that year from the df
+    year_data = data.loc[year_start:year_end, ['Sea Level']]
+# Sets all data in the new df as numeric
+    year_data = year_data.apply(pd.to_numeric, errors='raise')
+# Calculates mean and subtracts from all data points
+    year_data = (year_data)-(year_data.mean())
+    print(year_data)
     
-    return data
+    return (year_data)
+
+extract_single_year_remove_mean(1947, data)
+
+
 
 def extract_section_remove_mean(start, end, data):
-    year_data = data.loc[year_data_start:year_data_end, ['Tide']]
-    mmm = np.mean(year_data['Sea Level'])
-    year_data['Sea Level'] -= mmm
+    #year_data = data.loc[year_data_start:year_data_end, ['Tide']]
+    #mmm = np.mean(year_data['Sea Level'])
+    #year_data['Sea Level'] -= mmm
     
     return data
 
+
+
+
 def join_data(data1, data2):
-    formatted_data1 = read_tidal_data(data1)
-    formatted_data2 = read_tidal_data(data2)
-    tidal_data_set = pd.merge(formatted_data1, formatted_data2)
-    print(tidal_data_set)
+    #formatted_data1 = read_tidal_data(data1)
+    #formatted_data2 = read_tidal_data(data2)
+    #tidal_data_set = pd.merge(formatted_data1, formatted_data2)
+    #print(tidal_data_set)
     
     return 
+
+
+
 
 def sea_level_rise(data):
                                                      
     return 
 
+
+
+
 def tidal_analysis(data, constituents, start_datetime):
     
     return 
+
+
+
 
 def get_longest_contiguous_data(data):
     
     return 
 
-tidal_file = 'data/1947ABE.txt'
-tidal_file2 = 'data/1946ABE.txt'
 
-join_data(tidal_file, tidal_file2)
+
+
+
+
+
 
 
 if __name__ == '__main__': 
