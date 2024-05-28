ba32392e1b9c005386da0d9ad5ef8fb625319442
jp2113 <jp2113@york.ac.uk>
2024-05-28 15:33:27 +0100

    Saving code, fixxed some error (not sure how!)

--- tidal_analysis.py
+++ tidal_analysis.py
@@ -3,6 +3,7 @@
 #!/usr/bin/env python3
 
 # import the modules you need here
+import sys
 import datetime
 import argparse
 import glob
@@ -28,7 +29,7 @@ def read_tidal_data(filename):
     data = data.set_index("Datetime")
 # Removes uneeded columns
 # (https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.drop.html)
-    data = data.drop(columns = ['Date','Cycle', 'Residual'])
+    data = data.drop(columns = ['Date','Cycle','Residual'])
 # Replaces any value containing M,N,T with NaN, in the 'Sea Level' column
 # (https://jhill1.github.io/SEPwC.github.io/)
     data.replace(to_replace =".*M$", value={"Sea Level" : np.nan}, regex=True, inplace=True)
@@ -93,18 +94,18 @@ def sea_level_rise(data):
 # Returns the p-value corresponding to the slope
 # (https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.linregress.html)
     slope, _intercept, _r, p, _std_err = linregress(x,y)
-    return p
+    return slope, p
 
 
-def tidal_analysis(data, start_datetime):
+def tidal_analysis(data, constituents, start_datetime):
     """Completes Tidal analysis from constituents of amplitudes and time periods"""
 # Drops all null values in the subset
 # (https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.dropna.html)
     data = data.dropna(subset=["Sea Level"])
-    tide = uptide.Tides(['M2'])
+    tide = uptide.Tides(constituents)
 # Gathers data as seconds from start_datetime
 # (https://docs.python.org/3/library/datetime.html)
-    tide.set_initial_time(datetime.datetime(start_datetime))
+    tide.set_initial_time(start_datetime)
     seconds_since = (data.index.astype('int64').to_numpy()/1e9) - start_datetime.timestamp()
 # Tidal analysis to return amp, and pha for subset at specific number of seconds from epoch
     amp,pha = uptide.harmonic_analysis(tide, data['Sea Level'].to_numpy(), seconds_since)
@@ -117,37 +118,56 @@ def tidal_analysis(data, start_datetime):
 
 
 #def user_interface(station):
-    """Finds all txt files in directory given by user, joins them,
-       and prints to screen: M2,S2, and sea level rise"""
+#    """Finds all txt files in directory given by user, joins them,
+#       and prints to screen: M2,S2, and sea level rise"""
 # Finds all the text files in directory, and loads them in
+
+
+#if __name__ == '__main__':
+#    parser = argparse.ArgumentParser(
+#                     prog="UK Tidal analysis",
+#                     description="Calculate tidal constiuents and RSL from tide gauge data",
+#                     epilog="Copyright 2024, Joshua Proctor"
+#                     )
+#    parser.add_argument("directory",
+#    help="the directory containing txt files with data")
+#    parser.add_argument('-v', '--verbose',
+#                   action='store_true',
+#                    default=False,
+#                    help="Print progress")
+#    args = parser.parse_args()
+#    dirname = args.directory
+#    verbose = args.verbose
+
+
+#all_files = glob.glob(str(dirname) + "/*.txt")
 all_files = glob.glob("data/aberdeen/*.txt")
+
 formatted_files = []
 
 for file in all_files:
     file = read_tidal_data(file)
     formatted_files.append(file)
 
-full_file = pd.concat(formatted_files)
-full_file = full_file.sort_values(by='Datetime', ascending=True)
+
+ 
+#full_file = pd.concat(formatted_files)
+#full_file = full_file.sort_values(by='Datetime', ascending=True)
+
 
 #    return full_file
 
 #print (sea_level_rise(full_file))
-#print (tidal_analysis(full_file, ['M2'], (2000,1,1,0,0,0)))
+#print (tidal_analysis(full_file, ['M2'], (2000,1,1)))
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-                     prog="UK Tidal analysis",
-                     description="Calculate tidal constiuents and RSL from tide gauge data",
-                     epilog="Copyright 2024, Jon Hill"
-                     )
-    parser.add_argument("directory",
-    help="the directory containing txt files with data")
-    parser.add_argument('-v', '--verbose',
-                   action='store_true',
-                    default=False,
-                    help="Print progress")
-    args = parser.parse_args()
-    dirname = args.directory
-    verbose = args.verbose
+#full_file = full_file.dropna(subset=["Sea Level"])
+#tide=uptide.Tides(['M2'])
+#tide.set_initial_time(datetime.datetime(2000,1,1,0,0,0))
+#seconds_since = (full_file.index.astype('int64').to_numpy()/1e9) - datetime.datetime(2000,1,1,0,0,0).timestamp()
+#amp,pha = uptide.harmonic_analysis(tide, full_file['Sea Level'].to_numpy(), seconds_since)
+#print (amp,pha)
+
+
+
+
